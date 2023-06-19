49abbb1e5273542d8f769900dd82830d321eb85f
prakash deep sharma <42887999+prakashdeep2902@users.noreply.github.com>
2023-06-20 02:13:34 +0530

    Add fronten

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'
+import Insert from './component/InsertData/Insert'
+import GetData from './component/GetData/GetData'
+const App = () => {
+  return (
+   
+
+      <div className='maincontainer'>
+        <Insert></Insert>
+        <GetData></GetData>
+      </div>
+
+   
+  )
+}
+
+export default App
